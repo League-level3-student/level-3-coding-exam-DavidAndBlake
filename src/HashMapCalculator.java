3a406fea0cee9232259ec94e6108da186e001e06
alvincvms <>
2019-03-14 16:56:04 -0700

    couldn't make any progress
    I guess I'm supposed to use a for each loop

--- src/HashMapCalculator.java
+++ src/HashMapCalculator.java
@@ -4,14 +4,8 @@ public class HashMapCalculator {
 
 	public String commonKeyValuePairs(HashMap<String, String> hashmap1, HashMap<String, String> hashmap2) {
 		int numberOfMatches = 0;
-		for (int i = 0; i < hashmap1.size(); i++) {
-			for (int j = 0; j < hashmap2.size(); j++) {
-				if (hashmap1.get(i)==(hashmap2.get(j))) {
-					numberOfMatches++;
-				}
-			}
+		for(int i:hashmap1) {
+			
 		}
-		System.out.println(numberOfMatches);
-		return ("" + numberOfMatches);
 	}
 }
